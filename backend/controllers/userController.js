005734e0a4190ffa889005ddadaf7dc35021123a
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-04 09:48:49 +0530

    authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,10 @@
 import User from "../models/userModel.js";
 import bcrypt from 'bcrypt';
+import { generateToken } from "../utils/generateToken.js";
 
 
 const registerUser=async(req,res)=>{
+   try {
     const {name,email,password}=req.body;
     const userExists=await User.findOne({email});
 
@@ -20,7 +22,55 @@ const registerUser=async(req,res)=>{
 
     await user.save();
 
+    generateToken(req, res, user._id);
+
+    res.status(201).json({
+        message: 'Registration successful. Welcome!',
+        userId: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin
+      });
+   } catch (error) {
+    res.status(500).json({message:error.message});
+    
+   }
+
 
 }
 
-export {registerUser};
+
+const loginUser=async(req,res)=>{
+   try {
+    const {email,password}=req.body;
+
+    const user=await User.findOne({email});
+
+    if(!user){
+        res.status(400).json({message:"User does not exist"});
+    }
+
+    const isMatch=await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        res.status(400).json({message:"Invalid credentials"});
+    }
+
+    generateToken(req, res, user._id);
+
+    res.status(200).json({
+      message: 'Login successful.',
+      userId: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    });
+   } catch (error) {
+    res.status(500).json({message:error.message});
+    
+   }
+
+
+}
+
+export {registerUser,loginUser};
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 
-export const generateToekn=(req,res,userId)=>{
+export const generateToken=(req,res,userId)=>{
     const token=jwt.sign({userId},process.env.JWT_SECRET,{
         expiresIn:'30d'
     })
